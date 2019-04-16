27dee951ae27110f4e33e90a733ef6db6de2470d
Andrei Gaevsky <gaevskyandrei@gmail.com>
2019-04-16 09:57:05 +0300

    Fix some problems with a code format

---
+++ task3/servlets/src/com/bsu/exadel/servlets/JavaServletCheck.java
@@ -0,0 +1,15 @@
+package com.bsu.exadel.servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class JavaServletCheck extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("application/json");
+        resp.getOutputStream().println("{\"done\": true}");
+    }
+}
